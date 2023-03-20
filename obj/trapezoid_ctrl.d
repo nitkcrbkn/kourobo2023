257a90aea33f67ea1415196efc32f3eacb56022e
starkoka <j22437@inc.kisarazu.ac.jp>
2023-03-20 14:01:01 +0900

    動作テスト

--- obj/trapezoid_ctrl.d
+++ obj/trapezoid_ctrl.d
@@ -1,13 +1,9 @@
 obj/trapezoid_ctrl.o: App/Src/trapezoid_ctrl.c \
- /mnt/c/Users/Chiaki/Documents/sin_roboken/2rin/Drivers/SystemTasksManager/Inc/SystemTaskManager.h \
- /mnt/c/Users/Chiaki/Documents/sin_roboken/2rin/Drivers/DevDriver/RC/Inc/DD_RC.h \
- /mnt/c/Users/Chiaki/Documents/sin_roboken/2rin/Drivers/DevDriver/MD/Inc/DD_MD.h \
- /mnt/c/Users/Chiaki/Documents/sin_roboken/2rin/App/Inc/trapezoid_ctrl.h
-
-/mnt/c/Users/Chiaki/Documents/sin_roboken/2rin/Drivers/SystemTasksManager/Inc/SystemTaskManager.h:
-
-/mnt/c/Users/Chiaki/Documents/sin_roboken/2rin/Drivers/DevDriver/RC/Inc/DD_RC.h:
-
-/mnt/c/Users/Chiaki/Documents/sin_roboken/2rin/Drivers/DevDriver/MD/Inc/DD_MD.h:
-
-/mnt/c/Users/Chiaki/Documents/sin_roboken/2rin/App/Inc/trapezoid_ctrl.h:
+ /home/kokastar/nitkc_rbkn/kourobo2023/Drivers/SystemTasksManager/Inc/SystemTaskManager.h \
+ /home/kokastar/nitkc_rbkn/kourobo2023/Drivers/DevDriver/RC/Inc/DD_RC.h \
+ /home/kokastar/nitkc_rbkn/kourobo2023/Drivers/DevDriver/MD/Inc/DD_MD.h \
+ /home/kokastar/nitkc_rbkn/kourobo2023/App/Inc/trapezoid_ctrl.h
+/home/kokastar/nitkc_rbkn/kourobo2023/Drivers/SystemTasksManager/Inc/SystemTaskManager.h:
+/home/kokastar/nitkc_rbkn/kourobo2023/Drivers/DevDriver/RC/Inc/DD_RC.h:
+/home/kokastar/nitkc_rbkn/kourobo2023/Drivers/DevDriver/MD/Inc/DD_MD.h:
+/home/kokastar/nitkc_rbkn/kourobo2023/App/Inc/trapezoid_ctrl.h:

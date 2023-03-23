c1267ea286c99322147f792b4503a4d86f977ae8
starkoka <j22437@inc.kisarazu.ac.jp>
2023-03-24 01:49:05 +0900

    UpDownの時間制御を追加

--- App/Src/app.c
+++ App/Src/app.c
@@ -12,8 +12,8 @@
 /*suspensionSystem*/
 
 /*アーム関連の定数*/
-#define OUT_LIMIT 120000 /*duty値と時間(10ms単位)の積*/
-#define IN_LIMIT 0 /*同上*/
+#define AUTO_UP 100
+#define AUTO_DOWN -50
 #define AUTO_ARM_WIDTH 250
 #define MOTOR_SPEED_AMR 1000
 #define MOTOR_SPEED_UPDOWN 3000
@@ -27,6 +27,7 @@ int armSystem(void);
 
 static
 int upDownSystem(void);
+
 
 
 /* 腕振り部の変数 */
@@ -46,79 +47,79 @@ int checkpush = 0;
 
 #define WRITE_ADDR (const void*)(0x8000000+0x400*(128-1))/*128[KiB]*/
 flashError_t checkFlashWrite(void){
-  const char data[]="HelloWorld!!TestDatas!!!\n"
-    "however you like this microcomputer, you don`t be kind to this computer.";
-  return MW_flashWrite(data,WRITE_ADDR,sizeof(data));
+    const char data[]="HelloWorld!!TestDatas!!!\n"
+                      "however you like this microcomputer, you don`t be kind to this computer.";
+    return MW_flashWrite(data,WRITE_ADDR,sizeof(data));
 }
 
 int appInit(void){
-  message("msg","hell");
+    message("msg","hell");
 
-  /* switch(checkFlashWrite()){ */
-  /* case MW_FLASH_OK: */
-  /*   message("msg","FLASH WRITE TEST SUCCESS\n%s",(const char*)WRITE_ADDR); */
-  /*   break; */
-  /* case MW_FLASH_LOCK_FAILURE: */
-  /*   message("err","FLASH WRITE TEST LOCK FAILURE\n"); */
-  /*   break; */
-  /* case MW_FLASH_UNLOCK_FAILURE: */
-  /*   message("err","FLASH WRITE TEST UNLOCK FAILURE\n"); */
-  /*   break; */
-  /* case MW_FLASH_ERASE_VERIFY_FAILURE: */
-  /*   message("err","FLASH ERASE VERIFY FAILURE\n"); */
-  /*   break; */
-  /* case MW_FLASH_ERASE_FAILURE: */
-  /*   message("err","FLASH ERASE FAILURE\n"); */
-  /*   break; */
-  /* case MW_FLASH_WRITE_VERIFY_FAILURE: */
-  /*   message("err","FLASH WRITE TEST VERIFY FAILURE\n"); */
-  /*   break; */
-  /* case MW_FLASH_WRITE_FAILURE: */
-  /*   message("err","FLASH WRITE TEST FAILURE\n"); */
-  /*   break;         */
-  /* default: */
-  /*   message("err","FLASH WRITE TEST UNKNOWN FAILURE\n"); */
-  /*   break; */
-  /* } */
-  /* flush(); */
+    /* switch(checkFlashWrite()){ */
+    /* case MW_FLASH_OK: */
+    /*   message("msg","FLASH WRITE TEST SUCCESS\n%s",(const char*)WRITE_ADDR); */
+    /*   break; */
+    /* case MW_FLASH_LOCK_FAILURE: */
+    /*   message("err","FLASH WRITE TEST LOCK FAILURE\n"); */
+    /*   break; */
+    /* case MW_FLASH_UNLOCK_FAILURE: */
+    /*   message("err","FLASH WRITE TEST UNLOCK FAILURE\n"); */
+    /*   break; */
+    /* case MW_FLASH_ERASE_VERIFY_FAILURE: */
+    /*   message("err","FLASH ERASE VERIFY FAILURE\n"); */
+    /*   break; */
+    /* case MW_FLASH_ERASE_FAILURE: */
+    /*   message("err","FLASH ERASE FAILURE\n"); */
+    /*   break; */
+    /* case MW_FLASH_WRITE_VERIFY_FAILURE: */
+    /*   message("err","FLASH WRITE TEST VERIFY FAILURE\n"); */
+    /*   break; */
+    /* case MW_FLASH_WRITE_FAILURE: */
+    /*   message("err","FLASH WRITE TEST FAILURE\n"); */
+    /*   break;         */
+    /* default: */
+    /*   message("err","FLASH WRITE TEST UNKNOWN FAILURE\n"); */
+    /*   break; */
+    /* } */
+    /* flush(); */
 
-  ad_init();
+    ad_init();
 
-  message("msg","plz confirm\n%d\n",g_adjust.rightadjust.value);
+    message("msg","plz confirm\n%d\n",g_adjust.rightadjust.value);
 
-  /*GPIO の設定などでMW,GPIOではHALを叩く*/
-  return EXIT_SUCCESS;
+    /*GPIO の設定などでMW,GPIOではHALを叩く*/
+    return EXIT_SUCCESS;
 }
 
 /*application tasks*/
 int appTask(void){
     int ret=0;
 
-  /*if(__RC_ISPRESSED_R1(g_rc_data)&&__RC_ISPRESSED_R2(g_rc_data)&&
-     __RC_ISPRESSED_L1(g_rc_data)&&__RC_ISPRESSED_L2(g_rc_data)){
-    while(__RC_ISPRESSED_R1(g_rc_data)||__RC_ISPRESSED_R2(g_rc_data)||
-	  __RC_ISPRESSED_L1(g_rc_data)||__RC_ISPRESSED_L2(g_rc_data));
-    ad_main();
-    }*/
+    /*if(__RC_ISPRESSED_R1(g_rc_data)&&__RC_ISPRESSED_R2(g_rc_data)&&
+       __RC_ISPRESSED_L1(g_rc_data)&&__RC_ISPRESSED_L2(g_rc_data)){
+      while(__RC_ISPRESSED_R1(g_rc_data)||__RC_ISPRESSED_R2(g_rc_data)||
+        __RC_ISPRESSED_L1(g_rc_data)||__RC_ISPRESSED_L2(g_rc_data));
+      ad_main();
+      }*/
 
-  /*それぞれの機構ごとに処理をする*/
-  /*途中必ず定数回で終了すること。*/
-  ret = suspensionSystem();
-  if(ret){
-    return ret;
-  }
+    /*それぞれの機構ごとに処理をする*/
+    /*途中必ず定数回で終了すること。*/
+    ret = suspensionSystem();
+    if(ret){
+        return ret;
+    }
 
-  ret = upDownSystem();
-  if(ret){
-    return ret;
-  }
+    ret = upDownSystem();
+    if(ret){
+        return ret;
+    }
 
-  ret = armSystem();
-  if(ret){
-    return ret;
-  }
+    ret = armSystem();
+    if(ret){
+        return ret;
+    }
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
 
@@ -158,22 +159,22 @@ int suspensionSystem(void){
 
     /*for each motor*/
     for(i=0;i<num_of_motor;i++){
-      /*それぞれの差分*/
-      switch(i){
-      case 0:
-	idx = MECHA1_MD0;
-	rc_analogdata = DD_RCGetRY(g_rc_data);
-	break;
-      case 1:
-	idx = MECHA1_MD1;
-	rc_analogdata = -DD_RCGetLY(g_rc_data);
-	break;
+        /*それぞれの差分*/
+        switch(i){
+            case 0:
+                idx = MECHA1_MD0;
+                rc_analogdata = DD_RCGetRY(g_rc_data);
+                break;
+            case 1:
+                idx = MECHA1_MD1;
+                rc_analogdata = -DD_RCGetLY(g_rc_data);
+                break;
 
-      default:
-	return EXIT_FAILURE;
-      }
-      trapezoidCtrl(rc_analogdata * MD_GAIN,&g_md_h[idx],&tc);
-   }
+            default:
+                return EXIT_FAILURE;
+        }
+        trapezoidCtrl(rc_analogdata * MD_GAIN,&g_md_h[idx],&tc);
+    }
 
 
     return EXIT_SUCCESS;
@@ -234,20 +235,41 @@ int upDownSystem(void){
     unsigned int idx = 4;/*インデックス*/
     int i;
     int duty;
+    static int flagAutoUpDown = 0;
 
     const tc_const_t tc ={
             .inc_con = 100,
             .dec_con = 225,
     };
 
-    if(__RC_ISPRESSED_UP(g_rc_data)){
+    if(flagAutoUpDown > 0){ /*上昇中なら*/
+        flagAutoUpDown -= 1;
         duty = MOTOR_SPEED_UPDOWN;
     }
-    else if(__RC_ISPRESSED_DOWN(g_rc_data)){
+    else if(flagAutoUpDown < 0){ /*下昇中なら*/
+        flagAutoUpDown += 1;
         duty = MOTOR_SPEED_UPDOWN * -1;
     }
     else{
-        duty = 0;
+        if(__RC_ISPRESSED_RIGHT(g_rc_data)){
+            flagAutoUpDown = AUTO_UP;
+            duty = MOTOR_SPEED_UPDOWN;
+        }
+        else if(__RC_ISPRESSED_LEFT(g_rc_data)){
+            flagAutoUpDown = AUTO_DOWN;
+            duty = MOTOR_SPEED_UPDOWN * -1;
+        }
+        else if(__RC_ISPRESSED_UP(g_rc_data)){
+            flagAutoUpDown = 0;
+            duty = MOTOR_SPEED_UPDOWN;
+        }
+        else if(__RC_ISPRESSED_DOWN(g_rc_data)){
+            flagAutoUpDown = 0;
+            duty = MOTOR_SPEED_UPDOWN * -1;
+        }
+        else{
+            duty = 0;
+        }
     }
 
 
